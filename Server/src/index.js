2ef4def2540255e2c73a7ac506d8832c11b77265
PedroBarge <pedro525tavares@gmail.com>
2024-07-05 15:55:20 +0100

    fix server. wont send logs

--- Server/src/index.js
+++ Server/src/index.js
@@ -5,11 +5,11 @@
  */
 
 const udpServer = require('./udpServer');
-const WebSocketServer = require('./webSocketServer');
+const {app} = require('./webSocketServer');
 const {UDP_PORT,UDP_HOST, WS_PORT} = require('./config');
 
 udpServer.bind(UDP_PORT, UDP_HOST); 
 
-WebSocketServer.listen(WS_PORT, () => {
+app.listen(WS_PORT, () => {
     console.log(`WebSocket server running at http://localhost:${WS_PORT}`);
 })
--- Server/src/webSocketServer.js
+++ Server/src/webSocketServer.js
@@ -1,12 +1,12 @@
-const http = require('http');
-const socketIo = require('socket.io');
+const http = require("http");
+const socketIo = require("socket.io");
 
 const app = http.createServer();
 const io = socketIo(app, {
-    cors: {
-        origin: "http://localhost:5173",
-        methods: ["GET", "POST"]
-    }
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
 });
 
-module.exports = app;
+module.exports = { app, io };
